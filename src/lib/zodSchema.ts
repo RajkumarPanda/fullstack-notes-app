f3d8dea7ae9db27c7f3588b3c6c3f95e2c60887c
Rajkumar Panda <rajkumarpanda958@gmail.com>
2025-11-25 21:41:55 +0530

    add  the note schema by using zod

---
+++ src/lib/zodSchema.ts
@@ -0,0 +1,11 @@
+import z from "zod";
+
+export const noteFormInputSchema = z.object({
+	noteTitle: z
+		.string()
+		.min(5, { error: "The title must be 5 or more characters long" })
+		.max(50, { error: "The title must be 20 or less characters" }),
+	noteBody: z
+		.string()
+		.min(3, { error: "The content must be 10 or more characters" }),
+});
