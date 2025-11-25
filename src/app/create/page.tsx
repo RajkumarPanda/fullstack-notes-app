80deec278a4292b1de9e1231542e73e0b836e58d
Rajkumar Panda <rajkumarpanda958@gmail.com>
2025-11-26 03:12:22 +0530

    Invoke the noteForm component inside create page

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,5 +1,31 @@
+import NoteForm from "@/components/NoteForm";
+import {
+	Card,
+	CardContent,
+	CardHeader,
+	CardTitle,
+} from "@/components/shadcnui/card";
+import { Separator } from "@/components/shadcnui/separator";
+
 const page = () => {
-	return <div>create page</div>;
+	return (
+		<>
+			<div className="flex items-center justify-center">
+				<Card className="w-[600px]">
+					<CardHeader className="flex justify-center">
+						<CardTitle className="text-2xl font-semibold">
+							Add Your Note
+						</CardTitle>
+					</CardHeader>
+
+					<Separator />
+					<CardContent className="p-4">
+						<NoteForm />
+					</CardContent>
+				</Card>
+			</div>
+		</>
+	);
 };
 
 export default page;
